f9f40e3160f2cecd00bd703ef28b75bf98fcaf48
Jeff Williams <jwill1258@gmail.com>
2020-06-19 01:57:41 -0400

    About Me Update

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import { Card, CardText, CardTitle } from 'react-mdl';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 
-class About extends Component {
+class Resume extends Component {
   render() {
     return(
       <div>
+        <Grid>
+       
+          <Cell className="resume-right-col" col={8}>
+            <h2>About Me</h2>
 
-        <Card>
+            <p>
+              Hello! My name is Jeffrey Williams. I am from Orlando, Florida. I moved to Boca Raton to attend Florida Atlantic University in 2013 and lived there for 6 years. I worked mostly as a sales associate during college. I was able to apply my previous knowledge in bookkeeping, marketing, and finance to this position while being a full-time student and balancing multiple extracurricular activities. I graduated in 2017 with my Bachelor of Health Services in health administration.
+              <br></br>
+              After completing my undergraduate degree, I have been working as a credentialing coordinator for a fortune 500 company. Since starting at the company, I have worked directly under the operations director and was the taught the ins and outs of operations and finance (business partner roles). Health care is a good field to work in and I have been fortunate enough to work for a great organization, but I don’t feel that it is where I can reach my full potential. Even though I chose a different path in college, technology has always been a passion of mine. I am excited to put my unique perspective to working building some cutting edge technologies with/for you!            
+            </p>
+            
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
+
+
+
+
+
+        {/* <Card>
         <CardTitle>About Me</CardTitle>
             <CardText>
               Hello! My name is Jeffrey Williams. I am from Orlando, Florida. I moved to Boca Raton to attend Florida Atlantic University in 2013 and lived there for 6 years. I worked mostly as a sales associate during college. I was able to apply my previous knowledge in bookkeeping, marketing, and finance to this position while being a full-time student and balancing multiple extracurricular activities. I graduated in 2017 with my Bachelor of Health Services in health administration.
@@ -15,10 +41,4 @@ class About extends Component {
               After completing my undergraduate degree, I have been working as a credentialing coordinator for a fortune 500 company. Since starting at the company, I have worked directly under the operations director and was the taught the ins and outs of operations and finance (business partner roles). Health care is a good field to work in and I have been fortunate enough to work for a great organization, but I don’t feel that it is where I can reach my full potential. Even though I chose a different path in college, technology has always been a passion of mine. I am excited to put my unique perspective to working building some cutting edge technologies with/for you!            
             </CardText>
                 
-        </Card>
-      </div>
-    )
-  }
-}
-
-export default About;
+        </Card> */}
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import { Cell, Grid, List, ListItem, ListItemContent } from 'react-mdl';
 
 
 class Contact extends Component {
@@ -14,7 +14,9 @@ class Contact extends Component {
               alt="avatar"
               style={{height: '250px'}}
                />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Full stack web developer with a certificate from the University of Central Florida Full Stack Dev program. I also have a background in Graphic Design, Operations, and Finance, mostly in regards to how they correlate with maintaining systems and data for compliance, regulation, and advancing efficiency. While working in this realm, I understood I was intrinsically drawn to the process of helping a business grow through backend protocols and discovered a great passion for coding after assisting in the development of a methodology that increase workflow by over 15%. I am excited to bring my knowledge as well as JavaScript, PHP, Node.js, and MySQL skills to the right consumer facing company. 
+
+</p>
 
           </Cell>
           <Cell col={6}>
